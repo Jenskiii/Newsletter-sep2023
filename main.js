9aeef554598f5f06c1c83843de7725222c16f007
Jenskiii <124568361+Jenskiii@users.noreply.github.com>
2023-09-14 17:28:34 +0200

    updated submit page

--- main.js
+++ main.js
@@ -1,51 +1,73 @@
+//  form
 const form = document.getElementById("form");
 const email = document.getElementById("email");
-const forgroup = document.querySelector("form-group")
+const forgroup = document.querySelector("form-group");
+// cards
+const mainCard = document.querySelector(".main-card");
+const secondaryCard = document.querySelector(".secondary-card");
+const dismissButton = document.querySelector(".dismiss");
+//  goes back to main page on click
+dismissButton.addEventListener("click", hideSecondaryCard);
 
 
-form.addEventListener("submit",(e) => {
-    e.preventDefault();
-   console.log(e)
-
-    validateInputs();
-  });
+// starts validation on submit
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  e.stopImmediatePropagation()
+  validateInputs();
+});
 
 //  shows error if validation is false
-function setError(input, message) {
+const setError = (input, message) => {
   const formGroup = input.parentElement;
   const errorDisplay = formGroup.querySelector("small");
-  console.log(formGroup)
+
   errorDisplay.innerText = message;
   formGroup.classList.add("error");
 }
 
 //  removes errror if validation is true
-function setSucces(input) {
+const setSuccess = (input) => {
   const formGroup = input.parentElement;
   const errorDisplay = formGroup.querySelector("small");
-  console.log(formGroup)
+
   errorDisplay.innerText = "";
-  formGroup.classList.add("gay");
   formGroup.classList.remove("error");
 }
-
-// checks if valid email
-function isValidEmail() {
-  const regex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.test(String(email).toLowerCase());
+//  checks if value is valid email
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 //  validates user input
-function validateInputs() {
+const validateInputs = () => {
   // trim removes white spaces
   const emailValue = email.value.trim();
 
   if (emailValue === "") {
     setError(email, "Email is required");
   } else if (!isValidEmail(emailValue)) {
-    setError(email, "Valid email address required");
+    setError(email, "Invalid Email");
   } else {
-    setSucces(email);
+    setSuccess(email);
+    showSecondaryCard(emailValue);
+    email.value = "";
   }
+}
+
+function showSecondaryCard(value) {
+  // updates email on secondary card
+  const showEmail = document.querySelector(".secondary-card__email");
+  showEmail.innerText = value;
+
+  // show secondary card / hide main
+  secondaryCard.classList.remove("hidden");
+  mainCard.classList.add("hidden");
+}
+
+// hide secondary card / show main
+function hideSecondaryCard() {
+  secondaryCard.classList.add("hidden");
+  mainCard.classList.remove("hidden");
 }
