75c104af68972baf7e52c48794d140bd2db24922
Jenskiii <124568361+Jenskiii@users.noreply.github.com>
2023-09-14 15:51:17 +0200

    first upload test

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const forgroup = document.querySelector("form-group")
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+form.addEventListener("submit",(e) => {
+    e.preventDefault();
+   console.log(e)
+
+    validateInputs();
+  });
+
+//  shows error if validation is false
+function setError(input, message) {
+  const formGroup = input.parentElement;
+  const errorDisplay = formGroup.querySelector("small");
+  console.log(formGroup)
+  errorDisplay.innerText = message;
+  formGroup.classList.add("error");
+}
+
+//  removes errror if validation is true
+function setSucces(input) {
+  const formGroup = input.parentElement;
+  const errorDisplay = formGroup.querySelector("small");
+  console.log(formGroup)
+  errorDisplay.innerText = "";
+  formGroup.classList.add("gay");
+  formGroup.classList.remove("error");
+}
+
+// checks if valid email
+function isValidEmail() {
+  const regex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
+}
+
+//  validates user input
+function validateInputs() {
+  // trim removes white spaces
+  const emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    setError(email, "Email is required");
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, "Valid email address required");
+  } else {
+    setSucces(email);
+  }
+}
